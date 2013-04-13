f8ac8ed4c7c650e2d6f3a56fafe133e03cf15d3b
Matthew Bull <bull.matthew@gmail.com>
2013-04-14 00:09:55 +0100

    style changes to footer

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
             
         	<footer role="contentinfo" class="cf">
         	   
-            	<p class="attribution pull-left"><span class="highlighted">&copy;</span> <?php bloginfo('name'); ?> <?php echo date('Y'); ?></p>
+            	<p class="attribution pull-left"><span class="highlighted">&copy;</span> matthew bull <?php echo date('Y'); ?></p>
             	
             	<div class="link-block attribution pull-right">
                             
                     <ul>
-                        <li><a href="http://matthewbull.etsy.com">tumblr</a></li>
-                        <li><a href="http://matthewbull.etsy.com">facebook</a></li>
+                        <li><a href="http://gnozu.tumblr.com">tumblr</a></li>
+                        <li><a href="https://www.facebook.com/matthewbullphotography">facebook</a></li>
                         <li><a href="http://matthewbull.etsy.com">etsy</a></li>
                         <li class="last"><a href="http://matthewbull.net">blog</a></li>
                     </ul>
